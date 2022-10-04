29d1161f68df915ffcad35fc01bb914af71f295e
H.M Rayyan <hafizmuhammadrayyan@gmail.com>
2022-10-04 13:06:39 +0500

    Add react router on navbar

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,11 +1,70 @@
-import React from 'react'
+import React from 'react';
+import './index.css';
+
+// import All Components
+import Home from '../home';
+import Pricing from '../pricing';
+import About from '../about';
+import Contact from '../contact';
+import SignIn from '../sign in';
+
 import { SiRobotframework } from 'react-icons/si';
-import './index.css'
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
+
+
 
 const Navbar = () => (
 
     <div>
-        <nav className="navContainer">
+
+
+        <BrowserRouter>
+            <nav className="navContainer">
+
+                <div className="left">
+                    <SiRobotframework size='1.7em'></SiRobotframework>
+                    <span id='logoHeading'>Chatbot</span>
+                </div>
+
+                <div className="right">
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/pricing">Pricing</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About Us</Link>
+                        </li>
+                        <li>
+                            <Link to="/contact">Contact Us</Link>
+                        </li>
+                        <li>
+                            <Link to="/signIn"><button id='navBtn'>SIGN IN</button></Link>
+                        </li>
+                    </ul>
+                </div>
+
+            </nav>
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/pricing" element={<Pricing />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/signIn" element={<SignIn />} />
+            </Routes>
+
+        </BrowserRouter>
+
+
+        {/* <nav className="navContainer">
 
             <div className="left">
                 <SiRobotframework size='1.7em'></SiRobotframework>
@@ -22,7 +81,8 @@ const Navbar = () => (
                 </ul>
             </div>
 
-        </nav>
+        </nav> */}
+
     </div>
 )
 
